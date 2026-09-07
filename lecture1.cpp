b831cc09ebc1e7ddcc5d14cc0645ae2b44d1e204
Utkarsh Singh Rajput <utkarshsinghrajput121@gmail.com>
2026-09-07 16:13:55 +0000

    feat(cpp): add lecture 1 syntax

---
+++ lecture1.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // Preprocessor directive for input/output functions
+
+int main() {
+    // std::cout prints text, and std::endl moves to a new line
+    std::cout << "Hello World, Linux me swagat hai!" << std::endl; 
+    return 0; // Indicates successful execution
+}
