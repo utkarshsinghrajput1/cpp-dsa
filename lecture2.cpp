ab31afa236e8adc32e2bf269c05e8b69545c8f59
Utkarsh Singh Rajput <utkarshsinghrajput121@gmail.com>
2026-09-08 17:36:54 +0000

    feat(cpp): implement data types and variables basics

---
+++ lecture2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a = 123;
+
+    cout << a << endl;
+
+    char b = 'v';
+
+    cout << b << endl;
+
+    bool bl = true;
+    cout << bl << endl;
+
+    float f = 1.2;
+    cout << f << endl;
+
+    double d = 1.23456789;
+    cout << d << endl;
+
+    int size = sizeof(d);
+    cout << "Size of d is : " << size << endl;
+
+
+}
