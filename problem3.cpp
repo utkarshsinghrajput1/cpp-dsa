3324f981b88eda5af11dba105a3a56d01af611e0
Utkarsh Singh Rajput <utkarshsinghrajput121@gmail.com>
2026-09-11 01:00:09 +0000

    Sol: Solve problems on rational operators

---
+++ problem3.cpp
@@ -0,0 +1,22 @@
+ // Given two integers, n and m. The task is to check the relation between n and m. Print "less" if n < m,  "equal" if n == m, and "greater" if n > m.
+/* Example
+Input: n = 4, m = 8
+Output: less
+Explanation: 4 < 8 so print 'less'. */
+  solution:
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    if (n < m)
+        cout << "less";
+    else if (n == m)
+        cout << "equal";
+    else
+        cout << "greater";
+
+    return 0;
+}
