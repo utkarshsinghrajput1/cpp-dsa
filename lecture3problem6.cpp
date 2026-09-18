eccb1c9ed972c6ed812a2bc45baec87f8ba14b99
Utkarsh Singh Rajput <utkarshsinghrajput121@gmail.com>
2026-09-18 08:50:33 +0000

    Sol: Count number of zeros in a given number

---
+++ lecture3problem6.cpp
@@ -0,0 +1,32 @@
+// Q1. Number me kitne 0 hain? 🔢
+
+// Input: 1050200
+// Output: 4
+// Solution:
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;                         // [INPUT] Number store karega
+    int count = 0;                 // [COUNTER] Zero count karega
+
+    cout << "Enter number: ";
+    cin >> n;
+
+    while (n > 0)                  // [LOOP] Jab tak number khatam nahi hota
+    {
+        int digit = n % 10;        // [DIGIT] Last digit nikalo
+
+        if (digit == 0)             // [CHECK] Kya digit 0 hai?
+        {
+            count++;               // [UPDATE] Zero ki count increase
+        }
+
+        n = n / 10;                // [REMOVE] Last digit remove karo
+    }
+
+    cout << "Number of zeros = " << count;
+
+    return 0;
+}
