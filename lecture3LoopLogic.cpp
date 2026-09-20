babc96ab2eb9f136ce35ba15fc636e349fca307d
Utkarsh Singh Rajput <utkarshsinghrajput121@gmail.com>
2026-09-20 17:32:38 +0000

    Add: Notes on while loop structure

---
+++ lecture3LoopLogic.cpp
@@ -0,0 +1,59 @@
+// In codes me 5 patterns especially yaad rakhna padega:
+// 1. Initialization
+// 2. Condition
+// 3. Update
+// 4. Input/Output
+// 5. Termination
+// total → values add karne ke liye
+// count → kitni baar hua count karne ke liye
+// flag → yes/no condition remember karne ke liye
+// break → loop ko immediately stop karne ke liye
+// sentinel value (0, -1, etc.) → input khatam karne ke liye
+// while (condition)
+// {
+    // kaam
+
+    // update
+// }
+
+
+
+// dekh in type ke question ke kuch pattern he or inka fanda samjna boht important he simple tujhe kuch nhi karna bas tu step by step samjhle or ye pattern yaad rakhle fir tu easily ye question solve kar lega
+// Scenario question ko is format me convert kar:
+// INPUT
+//   ↓
+// INITIALIZE
+//   ↓
+// WHILE CONDITION
+//   ↓
+// CHECK / IF
+//   ↓
+// WORK
+//   ↓
+// UPDATE
+//   ↓
+// BACK TO WHILE
+//   ↓
+// STOP
+//   ↓
+// OUTPUT
+
+
+// Example: ATM question 
+// Amount input
+//      ↓
+// Balance = 10000
+//      ↓
+// while(amount != 0)
+//      ↓
+// amount <= balance ?
+//      ↓
+// balance = balance - amount
+//      ↓
+// next amount
+//      ↓
+// amount = 0
+//      ↓
+// STOP
+//      ↓
+// Final balance
